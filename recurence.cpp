a408de5bef72fb66bbf5a98bdb199ec9671c001a
techbenj2k1 <45093758+techbenj2k1@users.noreply.github.com>
2020-04-02 21:48:35 +0300

    Update recurence.cpp

--- recurence.cpp
+++ recurence.cpp
@@ -11,13 +11,14 @@ int main()
     cin>>x>>y;
     z=3*x-y;
     cout<<y<<" "<<x<<" "<<z<<" ";
-    while(z>1){
-        y=x;
+    while(z>1){ //parcurgem si construim termenul antecedent pana cand gasim primul termen egal cu 1
+        y=x; //interschimbarea valorilor;
         x=z;
         z=3*x-y;
         cout<<z<<" ";
     }
-        cout<<"1";
+        cout<<"1"; //primul termen al secventei, deoarce while-ul se opreste cand gaseste primul termen egal cu 1;
+            //Complexitate O(N)- algoritm liniar dpdv al timpului;
 
     return 0;
 }
