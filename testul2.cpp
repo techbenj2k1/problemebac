b49ba179a63796d68aef170c418ed38cca3675fd
techbenj2k1 <45093758+techbenj2k1@users.noreply.github.com>
2020-04-22 21:29:56 +0300

    Create testul2.cpp

---
+++ testul2.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+   ifstream f("bac.in");
+   int x, s=0;
+   f>>x;
+   s=x;
+  int smin=1000;
+   while(f>>x){
+       s+=x;
+
+    if(s<smin)
+    {
+        smin=s;
+    }
+    if(s>0)s=0;
+
+   }
+        cout<<smin;
+}
+
