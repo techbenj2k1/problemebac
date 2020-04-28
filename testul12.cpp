8b513ce0dc8ac3410505b13e65f0a6fad0fcef46
techbenj2k1 <45093758+techbenj2k1@users.noreply.github.com>
2020-04-28 03:41:06 +0300

    Update testul12.cpp

--- testul12.cpp
+++ testul12.cpp
@@ -26,5 +26,50 @@ int main()
 
 
  }
+//s3-3
+#include <bits/stdc++.h>
+#define MAX_N 10005
+
+using namespace std;
+
+ ifstream f("bac.in");
+
+using namespace std;
+
+
+
+int main()
+{
+ int x;
+    cin >>x;
+    bool ok=0;
+
+        cout<<x<<" ";
+        while(x>1){
+            if(x<11){
+                    x=x-1;
+                    ok=1;
+            }
+            if(x%2==0 and ok==0){
+               x=x/2;
+            }
+            else if(x%2==1 and ok==0){
+               x=x-1;
+            }
+
+            cout<<x<<" ";
+        }
+    }
+
+//SI-1
+int pDoi(int n){
+int p=1;
+    while(p<=n){
+        p*=2;
+    }
+    return p/2;
+}
+
+
 
 
