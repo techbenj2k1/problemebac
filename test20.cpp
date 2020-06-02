b3ce7c69c2753fd74fde9fe4133382e945bb3df8
techbenj2k1 <45093758+techbenj2k1@users.noreply.github.com>
2020-06-02 19:25:01 +0300

    Update test20.cpp

--- test20.cpp
+++ test20.cpp
@@ -20,3 +20,56 @@ int main() {
     }
 
 }
+#include <bits/stdc++.h>
+
+using namespace std;
+
+ifstream f("bac.in");
+//const int MASK=99;
+int vf[201];
+#define vf (vf+100)
+const int SIZE=201;
+int main() {
+    char s[101], t[101];
+    int j;
+    cin.getline(s,101);
+    for(unsigned int i=0; s[i]; i++){
+        if(s[i]!=',') continue;
+        if(s[i]==','){
+            j=i;
+            while(s[j]!=' '){
+                strcpy(t,s+j+1);
+                strcpy(s+j,t);
+            }
+        }
+    }
+        cout<<s;
+}
+#include <bits/stdc++.h>
+
+using namespace std;
+
+ifstream f("bac.in");
+const int SIZE=201;
+int main() {
+  int x, s, smax=-1;
+  int y;
+  f>>y;
+  s=y;
+  while(f>>x){
+        //cout<<x<< " ";
+    if((x+y)%2==0){
+        s+=y;
+        //cout<<y<<" ";
+    }
+    else{
+        s+=y;
+        if(s>smax) smax=s;
+        s=0;
+    }
+    y=x;
+  }
+    if(s>smax) smax=s;
+    cout<<smax;
+}
+
